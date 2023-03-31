1b7b6ea4f4f7a03401e6f81f52889ffbee6ab28b
Toufiquer <toufiquerabc@gmail.com>
2023-03-31 23:35:43 +0600

    register and logIn page complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LogIn from "./components/pages/LogIn/LogIn";
+import Register from "./components/Register/Register";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LogIn />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logoImg from "./../../assets/lws-logo-dark.svg";
+const Nav = () => {
+  return (
+    <>
+      {" "}
+      <nav class="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
+        <div class="max-w-7xl mx-auto">
+          <div class="flex justify-between h-16 items-center">
+            <img class="h-10" src={logoImg} alt="Logo" />
+            <ul>
+              <li class="text-white">
+                <Link to="/">Logout</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav> 
+    </>
+  );
+};
+
+export default Nav;
